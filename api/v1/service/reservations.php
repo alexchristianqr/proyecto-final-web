ba7b2848dd34672f46dbef3c9e65de417d878928
Kevin Guevara Rodriguez <144500638+kevinKVN00@users.noreply.github.com>
2024-07-18 01:30:29 -0500

    Feat/api (#4)
    * reservations
    
    * reservations .

--- api/v1/service/reservations.php
+++ api/v1/service/reservations.php
@@ -1,1 +1,62 @@
 <?php
+require_once "../database.php";
+
+// Función lista reservas
+function reservations()
+{
+  $db = new Database();
+  $db->connect();
+
+  $success = false;
+  $message = null;
+  $result = ["success" => $success];
+
+  $id_cliente = Request::json("id_cliente", null);
+  $id_habitacion = Request::json("id_habitacion", null);
+  $estado = Request::json("estado", null);
+
+  $params = [];
+  $sql = "SELECT r.*, c.nombres as cliente_nombres, c.apellidos as cliente_apellidos, 
+            h.descripcion as habitacion_descripcion, e.nombres as empleado_nombres, e.apellidos as empleado_apellidos 
+            FROM reservas r 
+            JOIN clientes cl ON r.id_cliente = cl.id
+            JOIN personas c ON cl.id_persona = c.id
+            JOIN habitaciones h ON r.id_habitacion = h.id
+            JOIN empleados emp ON r.id_empleado = emp.id
+            JOIN personas e ON emp.id_persona = e.id";
+  $condiciones = [];
+
+  if($id_cliente !== null){
+    $condiciones[] = "r.id_cliente = ?";
+    $params[] = $id_cliente;
+  }
+
+  if ($id_habitacion !== null) {
+    $condiciones[] = "r.id_habitacion = ?";
+    $params[] = $id_habitacion;
+  }
+
+  if ($estado !== null) {
+    $condiciones[] = "r.estado = ?";
+    $params[] = $estado;
+  }
+
+  if (count($condiciones) > 0) {
+    $sql .= " WHERE " . implode(" AND ", $condiciones);
+  }
+  return jsonResponse($result);
+}
+
+/* RUTA */
+// Procesar la solicitud
+$method = $_SERVER['REQUEST_METHOD'];
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uriSegments = explode('/', trim($uri, '/'));
+switch ($method) {
+  case 'POST':
+    if ($uriSegments[3] === 'reservations.php') reservations();
+    break;
+  default:
+    return jsonResponse(['status' => 'error', 'message' => 'Ruta no encontrada'], 404);
+}
+/* FIN RUTA */
